52386516bb962f5796dcf8b29887cad1be63b7f1
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-02-20 16:58:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,8 +81,8 @@ def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
 
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
+def turn_count
+  @board.count{|token| token == "X" || token == "O"}
 end
 
 def move(board, index, player)
